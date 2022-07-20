623ab74b3bcf5d52b2589fabac6518c2f35f7d75
Matt Dittes <mattdittes@gmail.com>
2022-07-19 20:48:19 -0400

    commit tree 68287dc412d44c12f1105ab0bbfb4932eb59a887

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    const copyCats = [...cats, "Broom"];
+    return copyCats;
+}
+function prependCat(){
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+function removeLastCat(){
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+function removeFirstCat(){
+    const lastCats = cats.slice(1);
+    return lastCats;
+}
